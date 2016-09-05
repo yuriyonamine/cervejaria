fd15f8325e7926e6bfb57d1ef0ba84727f70363d
Yuri Yonamine <infonamina@gmail.com>
2016-09-05 02:01:58 -0300

    [Refactoring] Improving javascript files

--- src/main/resources/static/javascripts/style.fast-register.js
+++ src/main/resources/static/javascripts/style.fast-register.js
@@ -1,53 +1,67 @@
-$(function(){
-	var modal = $('#styleRegisterModal');
-	var saveButton = modal.find('.js-modal-register-style-save-btn');	
-	var form = modal.find('form');
-	var url = form.attr('action');
-	var inputStyleName = $('#styleName');
-	var containerErrorMessage = $(".js-fast-register-style-message");
-	
-	form.on('submit', function(event){
-		event.preventDefault();
-	});
-	
-	modal.on('shown.bs.modal', onModalShow);
-	modal.on('hide.bs.modal', onModalClose);
-	saveButton.on('click', onSaveButtonClick);
-	
-	function onModalShow(){
-		inputStyleName.focus();
+var Brewer = Brewer || {};
+
+Brewer.FastStyleRegister = (function() {
+
+	function FastStyleRegister() {
+		this.modal = $('#styleRegisterModal');
+		this.saveButton = this.modal.find('.js-modal-register-style-save-btn');
+		this.form = this.modal.find('form');
+		this.url = this.form.attr('action');
+		this.inputStyleName = $('#styleName');
+		this.containerErrorMessage = $(".js-fast-register-style-message");
 	}
-	
-	function onModalClose(){
-		inputStyleName.val('');
-		containerErrorMessage.addClass('hidden');
-		form.find('.form-group').removeClass('has-error');
+
+	FastStyleRegister.prototype.begin = function() {
+		this.form.on('submit', function(event) {event.preventDefault();});
+		this.modal.on('shown.bs.modal', onModalShow.bind(this));
+		this.modal.on('hide.bs.modal', onModalClose.bind(this));
+		this.saveButton.on('click', onSaveButtonClick.bind(this));
 	}
-	
-	function onSaveButtonClick(){
-		var styleName = inputStyleName.val().trim();
-		
+
+	function onModalShow() {
+		this.inputStyleName.focus();
+	}
+
+	function onModalClose() {
+		this.inputStyleName.val('');
+		this.containerErrorMessage.addClass('hidden');
+		this.form.find('.form-group').removeClass('has-error');
+	}
+
+	function onSaveButtonClick() {
+		var styleName = this.inputStyleName.val().trim();
+
 		$.ajax({
-			url:url,
-			method: 'POST',
-			contentType: 'application/json',
-			data: JSON.stringify({'name': styleName}),
-			error: onSaveStyleError,
-			success: onSaveStyle
+			url : this.url,
+			method : 'POST',
+			contentType : 'application/json',
+			data : JSON.stringify({
+				'name' : styleName
+			}),
+			error : onSavedStyleError.bind(this),
+			success : onSavedStyle.bind(this)
 		});
 	}
-	
-	function onSaveStyleError(obj){
+
+	function onSavedStyleError(obj) {
 		var errorMessage = obj.responseText;
-		containerErrorMessage.removeClass('hidden');
-		containerErrorMessage.html('<span>'+errorMessage + '</span>');
-		form.find('.form-group').addClass('has-error');
+		this.containerErrorMessage.removeClass('hidden');
+		this.containerErrorMessage.html('<span>' + errorMessage + '</span>');
+		this.form.find('.form-group').addClass('has-error');
 	}
-	
-	function onSaveStyle(style){
+
+	function onSavedStyle(style) {
 		var combo = $('#style');
-		combo.append('<option value='+style.id+'>'+style.name+'</option>');
-		combo.val(style.id);		
-		modal.modal('hide');		
+		combo.append('<option value=' + style.id + '>' + style.name
+				+ '</option>');
+		combo.val(style.id);
+		this.modal.modal('hide');
 	}
+
+	return FastStyleRegister;
+}());
+
+$(function() {
+	var fastStyleRegister = new Brewer.FastStyleRegister();
+	fastStyleRegister.begin();
 });
